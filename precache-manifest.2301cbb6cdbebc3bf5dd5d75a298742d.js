e0c119ec7d7c06bcb38125a2ba1fc0206d70c1cc
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-09-01 01:15:47 +0700

    Updates

---
+++ precache-manifest.2301cbb6cdbebc3bf5dd5d75a298742d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b2ef2fc858cfbf0c562b1343218163",
+    "url": "/i-talent/index.html"
+  },
+  {
+    "revision": "077bf2a355ddce0e3648",
+    "url": "/i-talent/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c93a160b3caf3e6780",
+    "url": "/i-talent/static/js/2.e054d647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/i-talent/static/js/2.e054d647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077bf2a355ddce0e3648",
+    "url": "/i-talent/static/js/main.dc8f6ff4.chunk.js"
+  },
+  {
+    "revision": "5a5627f8c49cdf22239b",
+    "url": "/i-talent/static/js/runtime-main.70f07c7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/i-talent/static/media/logo.5d5d9eef.svg"
+  }
+]);
